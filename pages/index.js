ae2f5c6140707669ad5689f21d88e68461a6333f
Mihir Gupta <mihirgupta0900@gmail.com>
2022-06-16 14:44:40 +0530

    use correct hero

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
-import hero from "../assets/university.jpeg";
+import hero from "../assets/heroimage.png";
 import about from "../assets/about.png";
 import Link from "next/link";
 import { marketplaceAddress } from "../config";
