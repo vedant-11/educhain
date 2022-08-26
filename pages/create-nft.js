00587e5d8673d8e0da41b78675a867fde85a6732
vedant-11 <69393820+vedant-11@users.noreply.github.com>
2022-08-26 14:22:39 +0530

    added infura new

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -5,7 +5,9 @@ import { useRouter } from "next/router";
 import Web3Modal from "web3modal";
 import { Select } from "@chakra-ui/react";
 
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+const client = ipfsHttpClient(
+  "https://ipfs.infura.io:5001/2Dt1qwje70glWohGIJ9TDymgMP3"
+);
 
 import { marketplaceAddress } from "../config";
 
